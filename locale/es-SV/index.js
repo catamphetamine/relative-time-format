81029776908bd55802509bd9394bd6187ab8f284
catamphetamine <purecatamphetamine@gmail.com>
2019-01-13 18:29:29 +0300

    #3. Locales "day after tomorrow" / "day before yesterday"

---
+++ locale/es-SV/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+	locale: 'es-SV',
+	long: require('./long.json'),
+	short: require('../es/short.json'),
+	narrow: require('../es/narrow.json'),
+	quantify: require('../es/quantify')
+}
